5e863bfb9e088cd3c7c16552b3aedcedd71e501a
Rithvik8001 <1017rithvik@gmail.com>
2025-03-19 18:29:28 -0400

    Initial Commit

---
+++ Tingo/TingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TingoApp.swift
+//  Tingo
+//
+//  Created by Rithvik Pallamreddy on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct TingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
